53840a2b1daa82303fbc67aa9410a4b48ff4b31f
arasalt <tasalta@gmail.com>
2013-03-22 18:47:15 -0400

    added some new tool for generation of results

---
+++ src/edu/purdue/jsan/tools/Main.java
@@ -0,0 +1,18 @@
+package edu.purdue.jsan.tools;
+
+import java.util.StringTokenizer;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//Automator a=new Automator("Salta");
+		WriterprintsAuto ww=new WriterprintsAuto("Salta");// Amy, Lauren, JiHeyon
+		
+				
+
+	}
+
+}
